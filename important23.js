9b5708f16fe6fa15031d7df8309ff9eb5aa48cbf
tecnOTKARSH <73396787+tecnOTKARSH@users.noreply.github.com>
2020-10-24 22:59:56 +0530

    all intial file

---
+++ important23.js
@@ -0,0 +1,20 @@
+(function($){
+    $(function(){
+  
+      $('.sidenav').sidenav({
+          edge: 'right'
+      });
+      $('.parallax').parallax(
+        
+      );
+         $('.carousel').carousel();
+
+         $('.collapsible').collapsible();
+
+         $('.modal').modal();
+
+         
+  
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+  
